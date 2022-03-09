fbf39d20e3566380482d740a4bf3fb82e9a164d8
Merve Karabekmez <78443130+mervekrbkmz@users.noreply.github.com>
2022-03-09 14:09:25 +0300

    commit

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Product } from '../product/product';
 import { Observable, throwError } from 'rxjs';
-import { tap, catchError } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
 
 
 @Injectable()
