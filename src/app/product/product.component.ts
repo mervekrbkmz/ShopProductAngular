c51b85130f2baa51a4787764d05a859d5ad3dc69
Merve Karabekmez <78443130+mervekrbkmz@users.noreply.github.com>
2022-03-09 14:59:58 +0300

    Observable metoduyla verileri getirme.

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -2,9 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AlertifyService } from '../services/alertify.service';
 import { ProductService } from '../services/product.service';
 import { Product } from './product';
-
-// import swal from 'sweetalert';
-
+import swal from 'sweetalert';
 
 @Component({
   selector: 'app-product',
@@ -14,32 +12,23 @@ import { Product } from './product';
 })
 export class ProductComponent implements OnInit {
   //oninitin implemente edilmesiyle ngonInit oluşur.
-
-  constructor(private alertifyService: AlertifyService,private productService:ProductService) { }
+  constructor(private alertifyService: AlertifyService, private productService: ProductService) { }
   title = "Ürün Listesi"
   filterText = "";
-
   products: Product[];
   //burayı apiden gelen datayla doldurdum.
-
-
-
   //component açıldığında ngonİnit çalışır.
-  ngOnInit(): void {
-    // //product nesnesinin product tipinde olduğunu map ettim.
-    // this.http.get<Product[]>(this.path)
-    //   .subscribe(data => { //dataya n'apıcağımı yazdım. dataya subscribe oldum.
-    //     this.products = data;
-    //   }); //subscribe ile ilgili datayı istediğimizi belirtir.
-    this.productService.getProducts().subscribe(data=>this.products=data)
-
+  ngOnInit() {
+    this.productService.getProducts().subscribe(data => {
+      this.products = data
+    });
   }
   addToCard(product: Product) {
 
-    // swal({
-    //   text:"Sepete Eklendi : "+ product.name,
-    //   icon: "success",
-    // });
+    swal({
+      text: "Sepete Eklendi : " + product.name,
+      icon: "success",
+    });
     this.alertifyService.success(product.name + "Added")
 
   }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,23 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Product } from '../product/product';
-import { Observable, throwError } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
 
 
 @Injectable()
 export class ProductService {
 
   constructor(private http: HttpClient) { }
-  path = "http://localhost:3344/products";
+  path = "http://localhost:3000/products";
 
   getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.path).pipe(
-      tap(data => console.log(JSON.stringify(data))),
-      //catcherror yazıcam-
-      ); 
-    }
-  
+    return this.http.get<Product[]>(this.path);
+  }
 }
-
-
