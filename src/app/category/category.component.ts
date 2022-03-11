0b0a80d41ac7fba72df04949fc848c99628c76d8
Merve Karabekmez <78443130+mervekrbkmz@users.noreply.github.com>
2022-03-11 11:46:59 +0300

    commit

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -6,13 +6,16 @@ import { CategoryService } from '../services/category.service';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
+  providers:[CategoryService]
 })
 export class CategoryComponent implements OnInit {
-  constructor(private categoryService:CategoryService,private alertifyService:AlertifyService) { }
+  constructor(private categoryService:CategoryService) { }
   title= "Kategori Listesi"
   categories: Category[];
   ngOnInit(): void {
-    
+    this.categoryService.getCategories().subscribe(data=>{
+      this.categories=data;
+    })
   }
 }
