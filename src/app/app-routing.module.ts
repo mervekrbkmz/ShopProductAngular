fe6bdd97ce815e7b21181934415953d126d3395b
Merve Karabekmez <78443130+mervekrbkmz@users.noreply.github.com>
2022-03-13 15:05:08 +0300

    Routing İşlemi-Products

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Product } from './product/product';
+import { ProductComponent } from './product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'products',component:ProductComponent}, //route tanımlaması yaptım.
+{path:'',redirectTo:'products',pathMatch: 'full'} //hiçbir path verilmemişse productsa redirect(yönlendirilme) edildi.
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule] //dışarıdan kullanıalbiir demek.
 })
 export class AppRoutingModule { }
